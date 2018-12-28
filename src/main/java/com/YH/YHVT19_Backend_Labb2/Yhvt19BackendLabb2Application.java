1d6710db7db96b41a54a654a254b36440bbcfb4d
kjelldg <Kjelldg@gmail.com>
2018-12-28 15:39:17 +0100

    Initial commit.

---
+++ src/main/java/com/YH/YHVT19_Backend_Labb2/Yhvt19BackendLabb2Application.java
@@ -0,0 +1,14 @@
+package com.YH.YHVT19_Backend_Labb2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Yhvt19BackendLabb2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Yhvt19BackendLabb2Application.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/YH/YHVT19_Backend_Labb2/Yhvt19BackendLabb2ApplicationTests.java
@@ -0,0 +1,17 @@
+package com.YH.YHVT19_Backend_Labb2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Yhvt19BackendLabb2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
