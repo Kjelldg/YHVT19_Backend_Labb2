2150d61a7b4a720444163dbf1d3c6b2cb3647a10
kjelldg <Kjelldg@gmail.com>
2018-12-29 16:32:38 +0100

    Added classes.

---
+++ src/main/java/com/YH/YHVT19_Backend_Labb2/Main.java
@@ -0,0 +1,13 @@
+package com.YH.YHVT19_Backend_Labb2;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		/*
+		 * 
+		 */
+
+	}
+
+}
---
+++ src/main/java/com/YH/YHVT19_Backend_Labb2/UserDao.java
@@ -0,0 +1,5 @@
+package com.YH.YHVT19_Backend_Labb2;
+
+public class UserDao {
+
+}
